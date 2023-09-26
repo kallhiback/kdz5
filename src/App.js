c493b84a808d1c8bc4edc8216a98b98ae66bbb96
kallhiback <130337500+kallhiback@users.noreply.github.com>
2023-09-26 21:31:02 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [form,setForm] = useState({
+    key1: 'Barbara',
+    key2: 'Hepworth',
+    key3: 'bhepworth@react.com',
+    key4: 'love cats'
+  });
+
+  function handleNameChange(e) {
+    setForm({
+      ...form,
+      key1: e.target.value
+    })
+  }
+
+
+  function handleSurnameChange(e) {
+    setForm({
+      ...form,
+      key2: e.target.value
+    })
+  }
+
+  function handleEmailChange(e) {
+    setForm({
+      ...form,
+      key3: e.target.value
+    })
+  }
+
+  function handleInteresChange(e) {
+    setForm({
+      ...form,
+      key4: e.target.value
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <h1
+            style={{
+                width: "180px",
+                textAlign: "center"
+            }}
+        >Form</h1>
+        <form
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+            border: "3px solid blue",
+            width: "180px",
+            padding: "10px",
+            borderRadius: "20px"
+        }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <label>
+            Name:<br/>
+            <input value={form.key1} onChange={handleNameChange}  type="text"/>
+          </label>
+          <label>
+            Surname:<br/>
+            <input value={form.key2} onChange={handleSurnameChange} type="text" />
+          </label>
+          <label>
+            Email:<br/>
+            <input value={form.key3} onChange={handleEmailChange} type="email"/>
+          </label>
+          <label>
+            Interests:<br/>
+            <input value={form.key4} onChange={handleInteresChange} type="text"/>
+          </label>
+        </form>
+        <div className="data-display"
+            style={{
+                width: "220px",
+                marginTop: "20px",
+                border: "3px solid lime",
+                borderRadius: "20px",
+                paddingLeft: "10px",
+
+            }}
+        >
+          <p><strong>Имя:</strong> {form.key1}</p>
+          <p><strong>Фамилия:</strong> {form.key2}</p>
+          <p><strong>Email:</strong> {form.key3}</p>
+          <p><strong>Интересы:</strong> {form.key4}</p>
+        </div>
+      </div>
   );
 }
 
